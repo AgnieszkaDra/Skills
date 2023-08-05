14954bba7df672a2de6f18087fc660564c33e460
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-08-05 13:12:38 +0200

    ad how tow show values in percentace

--- app.js
+++ app.js
@@ -1,141 +1,175 @@
-import Canvas from "./classes/Canvas/Canvas.js"
+const ctxToday = document.getElementById('myChartToday');
+const HTML = document.querySelector('.HTMLData')
+const htmlVal = document.querySelector('.html')
+console.log(htmlVal.value)
+const val5 = function getVal(){
+    alert('val')
+  const HTMLData = document.querySelector('.html').value;
 
-const form1 = document.querySelector('form')
+const HTMLDataNumber = Number(HTMLData)
+return console.log(HTMLData)
+}
 
-form1.addEventListener('submit', function(el){
-  el.preventDefault()
 
-  const dataToday = {
-    labels: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-      'React',
-    ],
-    datasets: [{
-      label: 'My Skills',
-      data: [],
-      
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)',
-        'rgb(456,19,456'
-      ],
-    }]
-  };
+
+ 
+ 
+
+
+
 
   
-   const HTMLVal = document.getElementById('1').value
- 
-   dataToday.datasets[0].data[0] = HTMLVal
-   const CSSVal = document.getElementById('2').value
-   dataToday.datasets[0].data[1] = CSSVal
-   const JSVal = document.getElementById('3').value
-   dataToday.datasets[0].data[2] = JSVal
-   const ReactVal = document.getElementById('4').value
-   dataToday.datasets[0].data[3] = ReactVal
-
-  const canvas = new Canvas('.skills')
-  canvas.init()
-
-const datapoint = ['HTML','CSS','JavaScript','React',]
+  
+ Chart.register(ChartDataLabels);
+  const datapoint = ['HTML','CSS','JavaScript','React',]
 let total = 0
-const ctxToday = document.getElementById('myChartToday');
-
+  
   const chart = new Chart(ctxToday,{
     type: 'pie',
-    data: dataToday,
-  //   options: {
-  //       plugins: {
-  //         tooltip: {
-  //           enabled: false, 
-  //         },
-  //           legend: {
-  //           position: 'bottom',
-  //           align: 'start',
-  //           onHover: false,
-  //           onLeave: false,
-  //       },
-  //       datalabels: {
-  //           // formatter: function(value, context) {
-  //           //     console.log(context)
-  //           //     console.log(context.chart.data.datasets[0].data)
-  //           //     console.log(context.chart.data.labels[context.dataIndex])
-  //           //     console.log(value)
-  //           //     //return context.chart.data.labels[context.dataIndex];
+    data: {
+ 
+      labels: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React',
+      ],
+      datasets: [{
+        label: 'My Skills',
+        data: [1, 1, 1,1 ],
+        
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)',
+          'rgb(456,19,456'
+        ],
+      }]
+    },
+    options: {
+        plugins: {
+          tooltip: {
+            enabled: false, 
+          },
+            legend: {
+            position: 'bottom',
+            align: 'start',
+            onHover: false,
+            onLeave: false,
+        },
+        datalabels: {
+            formatter: function(value, context) {
+                console.log(context)
+                console.log(context.chart.data.datasets[0].data)
+                console.log(context.chart.data.labels[context.dataIndex])
+                console.log(value)
+                //return context.chart.data.labels[context.dataIndex];
                
-  //           //     function totalSum(total, datapoint){
-  //           //       alert('sum')
-  //           //       return total + datapoint
-  //           //     }
+                function totalSum(total, datapoint){
+                  return total + datapoint
+                }
 
-  //           //    //   const datapoints1 = context.chart.data.datasets[0].data[0]
-  //           //     //   const datapoints2 = context.chart.data.datasets[0].data[1]
-  //           //     const datapoints = context.chart.data.datasets[0].data
-  //           //     console.log(datapoints)
+               //   const datapoints1 = context.chart.data.datasets[0].data[0]
+                //   const datapoints2 = context.chart.data.datasets[0].data[1]
+                const datapoints = context.chart.data.datasets[0].data
+                console.log(datapoints)
                 
-  //           //     const totalvalue = datapoints.reduce(totalSum,0)
-  //           //     console.log(totalvalue)
-  //           //     const percentageValue = (value/totalvalue *100).toFixed(1)
+                const totalvalue = datapoints.reduce(totalSum,0)
+                console.log(totalvalue)
+                const percentageValue = (value/totalvalue *100).toFixed(1)
                 
-  //           //     return ` ${percentageValue}%`
+                return ` ${percentageValue}%`
                 
-  //           //   }
-          
-  //           formatter: function(value, context) {
-              
-  //             return context.chart.data.labels[context.dataIndex];
-  //           }
-  //       },
+              }
+          },
+        },
 
-  //       events: ['mousemove', 'mouseout', 'click', 'touchstart', 'touchmove'],
-  //        'onClick' : function (evt, item) {
-  //         alert('click')
-  //           console.log ('legend onClick', evt);
-  //           console.log('legd item', item[0].element.options);
-  //            customCanvasBackgroundColor = 'lightGreen',
-  //             text = 'ala'
-  //             color= 'black'
+        events: ['mousemove', 'mouseout', 'click', 'touchstart', 'touchmove'],
+         'onClick' : function (evt, item) {
+            console.log ('legend onClick', evt);
+            console.log('legd item', item[0].element.options);
+             customCanvasBackgroundColor = 'lightGreen',
+              text = 'ala'
+              color= 'black'
               
-  //           item[0].element.options.backgroundColor = customCanvasBackgroundColor
-  //           item[0].element.options.borderColor= color
+            item[0].element.options.backgroundColor = customCanvasBackgroundColor
+            item[0].element.options.borderColor= color
             
         
-  //       },
-  //       'onHover' : function (evt, item) {
-  //           console.log ('legend onClick', evt);
-  //           console.log('legd item',  item);
-  //            customCanvasBackgroundColor = 'lightGreen',
-  //             text = 'ala'
+        },
+        'onHover' : function (evt, item) {
+            console.log ('legend onClick', evt);
+            console.log('legd item',  item);
+             customCanvasBackgroundColor = 'lightGreen',
+              text = 'ala'
               
           
-  //       },
+        },
        
 
-  //   }, 
+    }, 
   
-  // }
-})
-   }
-   )
    
-  
+ }
+
+
+  )
+
+
+
+
+
+const HTMLVal = document.getElementById('1')
+
+HTMLVal.addEventListener('input', function (el) {
+
+const valHTML = el.target.value
+  chart.data.datasets[0].data[0] = Number(valHTML)
+    chart.update()
+
+
+
+});
+
+const CSSVal = document.getElementById('2')
+
+CSSVal.addEventListener('input', function (el) {
+
+const valCSS = el.target.value
+  chart.data.datasets[0].data[1] = Number(valCSS)
+    chart.update()
+
+
+
+});
+
+
+const JSVal = document.getElementById('3')
+
+CSSVal.addEventListener('input', function (el) {
+
+const valJS = el.target.value
+  chart.data.datasets[0].data[2] = Number(valJS)
+    chart.update()
+
+
+
+});
+
+
+const ReactVal = document.getElementById('4')
+
+CSSVal.addEventListener('input', function (el) {
+
+const valReact = el.target.value
+  chart.data.datasets[0].data[3] = Number(valReact)
+    chart.update()
+
+
+
+});
+
+
+
 
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
